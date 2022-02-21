97f1fc10010a27446bac7108137615cc29b8c562
BharatiRuchika <ruchikabharaticdac@gmail.com>
2022-02-21 11:18:20 +0530

    sda

---
+++ Guvi/Kahoot_Frontend_Project-main/src/Socket.js
@@ -0,0 +1,14 @@
+import {io} from 'socket.io-client';
+// let socket = io("http://localhost:5000");
+// var socket = io.connect('https://kahhotbackendapp.herokuapp.com', {reconnect: true});
+var connectionOptions = {
+	"force new connection" : true,
+	"reconnect":true,
+	"reconnectionAttempts": "Infinity",
+	"timeout" : 10000,				
+	"transports" : ["websocket"]
+};
+let socket = io.connect('https://my-kahoot-backend.herokuapp.com/',connectionOptions);
+
+console.log("socket",socket);
+export default socket;
